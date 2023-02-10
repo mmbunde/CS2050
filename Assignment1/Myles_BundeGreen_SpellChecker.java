8074382abff947ec18f78e632a7cfd877c22351b
mmbunde <mmbunde@duck.com>
2023-02-09 21:39:02 -0700

    Finish adding the spell checker functionality

--- Assignment1/Myles_BundeGreen_SpellChecker.java
+++ Assignment1/Myles_BundeGreen_SpellChecker.java
@@ -1,19 +1,38 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class Myles_BundeGreen_SpellChecker{
 
     public static void main(String[] args){
         BagInterface<String> myDictionary = new Myles_BundeGreen_WordsBag<>();
-        int wordCount;
-        Scanner fileData = new Scanner(new File(input file));
+        int wordCount = 0;
+        Scanner keyboard = new Scanner(System.in);
+        File inputFile = new File("dictionary.txt");
+        
         try{
+            Scanner fileData = new Scanner(inputFile);
             while(fileData.hasNextLine()){
                 String line = fileData.nextLine();
                 if(myDictionary.add(line)){
                     wordCount++;
                 }
             }
+            fileData.close();
+            System.out.println(wordCount);
         }
+         catch(FileNotFoundException e){
+            System.out.println("File not found");
+         }
+
+         System.out.println("Please enter a word to check:");
+         String word = keyboard.nextLine();
+
+            if(myDictionary.contains(word)){
+                System.out.println("You have spelled " + word + " correctly");
+            }
+            else{
+                System.out.println("You have spelled " + word + " wrong.");
+            }
         
     }
 }
