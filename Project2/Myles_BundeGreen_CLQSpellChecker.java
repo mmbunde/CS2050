a2a97cfdbe3f70dee8be6fc00ea29bb0b36b37fc
mmbunde <mmbunde@duck.com>
2023-03-13 12:49:47 -0600

    first commit

---
+++ Project2/Myles_BundeGreen_CLQSpellChecker.java
@@ -0,0 +1,29 @@
+public class Myles_BundeGreen_CLQSpellChecker {
+    
+
+
+    /*Assignment2 with the file input but need to be able change
+    from push to enqueue
+
+    
+    StackInterface<String> myStackDictionary = new Myles_BundeGreen_Words_Stack<>();
+            int wordCount = 0;
+            Scanner keyboard = new Scanner(System.in);
+            File inputFile = new File("dictionary.txt");
+
+            try{
+                Scanner fileData = new Scanner(inputFile);
+                while(fileData.hasNextLine()){
+                    String line = fileData.nextLine();
+                    if(myStackDictionary.push(line)){
+                        wordCount++;
+                    }
+                }
+            }
+            catch(FileNotFoundException e){
+                System.out.println("File not found");
+            }
+            boolean check = true;
+            StackInterface<String> temp = new Myles_BundeGreen_Words_Stack<>();
+     */
+}
